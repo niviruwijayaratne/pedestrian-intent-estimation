e28f6bf6c08869aa2d084a8b7d52515ab68938b9
niviruwijayaratne <nnw2@illinois.edu>
2020-10-19 02:03:06 +0000

    container built, need to fix dataloaders

--- Optical Flow/flownet-pytorch/launch_docker.sh
+++ Optical Flow/flownet-pytorch/launch_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo nvidia-docker build -t $USER/flownet2:latest .
-sudo nvidia-docker run --rm -ti --volume=$(pwd):/flownet2-pytorch:rw --workdir=/flownet2-pytorch --ipc=host $USER/flownet2:latest /bin/bash
+echo $(pwd)
+sudo nvidia-docker run --rm -ti --volume=/home/ubuntu/niviru/poseflow-pie/:/poseflow-pie:rw --workdir=/poseflow-pie --ipc=host $USER/flownet2:latest /bin/bash
