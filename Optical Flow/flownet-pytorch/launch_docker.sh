7f48f00d478c6e3ac61cd8d23a0d66be550efaf3
niviruwijayaratne <nnw2@illinois.edu>
2020-12-10 23:42:20 +0000

    restructure/saved trained model

--- Optical Flow/flownet-pytorch/launch_docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo nvidia-docker build -t $USER/flownet2:latest .
-echo $(pwd)
-sudo nvidia-docker run --rm -ti --volume=/home/ubuntu/niviru/poseflow-pie/:/poseflow-pie:rw --workdir=/poseflow-pie --ipc=host $USER/flownet2:latest /bin/bash
